ca7c1eabb013fbabf3de93707bd6ac77643b241b
snxakioi <110209982+snxakioi@users.noreply.github.com>
2022-09-09 19:15:57 +0800

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,9 +1,9 @@
 var settings = {
-    "url": "https://beishifengqiban.cf//api/stats",
-    "method": "GET",
-    "timeout": 0,
-  };
-  $.ajax(settings).done(function (response) {
-    console.log(response.counter);
-    document.getElementById("SiteCounter").innerHTML="现在已有"+homo(response.counter)+"="+response.counter+"个人访问了本站";
-  });
+  "url": "https://beishifengqiban.cf//api/stats",
+  "method": "GET",
+  "timeout": 0,
+};
+$.ajax(settings).done(function (response) {
+  console.log(response.counter);
+  document.getElementById("SiteCounter").innerHTML="现在已有"+response.counter+"个人访问了本站";
+});
